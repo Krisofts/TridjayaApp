2b9e8dbbfcb998e967803caa5babb4ebc85416aa
dandi-apriadi <dandiapeiadi22@gmail.com>
2026-07-23 23:03:16 +0700

    chore(release): bump versi 2.7 (redesain approval aki single-approver)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.krisoft.tridjayaelektronik"
         minSdk = 24
         targetSdk = 35
-        versionCode = 17
-        versionName = "2.6"
+        versionCode = 18
+        versionName = "2.7"
 
         // Gateway Rust tridjaya, deployed at tridjaya.com (HTTPS, no emulator/LAN
         // workaround needed since it's a public domain). Migrated 2026-07-13 from
