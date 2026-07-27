400324566e0bdcdfa44363343681aaa89d999b0e
dandi-apriadi <dandiapeiadi22@gmail.com>
2026-07-27 18:53:43 +0700

    @ chore(release): bump versi 2.26 (timeline SPK dari server) @

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.krisoft.tridjayaelektronik"
         minSdk = 24
         targetSdk = 35
-        versionCode = 36
-        versionName = "2.25"
+        versionCode = 37
+        versionName = "2.26"
 
         // Gateway Rust tridjaya, deployed at tridjaya.com (HTTPS, no emulator/LAN
         // workaround needed since it's a public domain). Migrated 2026-07-13 from
