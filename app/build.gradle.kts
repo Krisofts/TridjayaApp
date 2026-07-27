bebb171e880628a57fbd889e3872d9330fb67895
dandi-apriadi <dandiapeiadi22@gmail.com>
2026-07-27 15:25:20 +0700

    @ chore(release): bump versi 2.25 (fix keyboard login + posisi tombol konfirmasi foto) @

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.krisoft.tridjayaelektronik"
         minSdk = 24
         targetSdk = 35
-        versionCode = 35
-        versionName = "2.24"
+        versionCode = 36
+        versionName = "2.25"
 
         // Gateway Rust tridjaya, deployed at tridjaya.com (HTTPS, no emulator/LAN
         // workaround needed since it's a public domain). Migrated 2026-07-13 from
