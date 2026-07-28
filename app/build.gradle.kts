69898bab302a3117714e31e1107724f41f9cf9ce
dandi-apriadi <dandiapeiadi22@gmail.com>
2026-07-28 15:39:11 +0700

    chore(release): bump versi 2.29 (fix pesan Cek Pembaruan)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.krisoft.tridjayaelektronik"
         minSdk = 24
         targetSdk = 35
-        versionCode = 39
-        versionName = "2.28"
+        versionCode = 40
+        versionName = "2.29"
 
         // Gateway Rust tridjaya, deployed at tridjaya.com (HTTPS, no emulator/LAN
         // workaround needed since it's a public domain). Migrated 2026-07-13 from
