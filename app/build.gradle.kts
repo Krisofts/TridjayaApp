5e2e6953b47ef10a0b209a53f6681a6e7ac97d1a
dandi-apriadi <dandiapeiadi22@gmail.com>
2026-07-28 16:52:25 +0700

    chore(release): bump versi 2.30 (GPS cepat, Pengaturan, Activity berikon)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.krisoft.tridjayaelektronik"
         minSdk = 24
         targetSdk = 35
-        versionCode = 40
-        versionName = "2.29"
+        versionCode = 41
+        versionName = "2.30"
 
         // Gateway Rust tridjaya, deployed at tridjaya.com (HTTPS, no emulator/LAN
         // workaround needed since it's a public domain). Migrated 2026-07-13 from
