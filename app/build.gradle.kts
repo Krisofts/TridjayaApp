1d4d4367a16a35db195536c41d61e49d1672f986
dandi-apriadi <dandiapeiadi22@gmail.com>
2026-07-29 10:45:27 +0700

    chore(release): bump versi 2.38 (fix crash Android 7 di hint Inventory, ubin Cari Semua)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.krisoft.tridjayaelektronik"
         minSdk = 24
         targetSdk = 35
-        versionCode = 48
-        versionName = "2.37"
+        versionCode = 49
+        versionName = "2.38"
 
         // Gateway Rust tridjaya, deployed at tridjaya.com (HTTPS, no emulator/LAN
         // workaround needed since it's a public domain). Migrated 2026-07-13 from
